1d4d6120153608ed9e781bc14765f90f048b6b46
Leandro Cieri <lcieri@domingoalonsogroup.com>
2022-05-25 09:31:17 +0100

    Refactor

--- PointOfSaleKata/UnitTest1.cs
+++ PointOfSaleKata/UnitTest1.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace PointOfSaleKata
@@ -43,25 +43,25 @@ namespace PointOfSaleKata
         {
             Assert.AreEqual("$19,75", pointOfSale.Scan("12345,23456"));
         }
+
+        [Test]
+        public void Return_sum_of_scanned_items3()
+        {
+            Assert.AreEqual("$27,00", pointOfSale.Scan("12345,23456,12345"));
+        }
+
+        [Test]
+        [TestCase("Error: empty barcode", "12345,,12345")]
+        [TestCase("Error: barcode not found", "12345,99999,12345")]
+        public void Return_error_of_scanned_items(string expected, string barcodes)
+        {
+            Assert.AreEqual(expected, pointOfSale.Scan(barcodes));
+        }
+        
     }
 
     public class PointOfSale
     {
-        public string Scan(string barcode)
-        {
-            var itemsPrices = ItemsPrices();
-            var items = barcode.Split(',');
-            var totalPrice = 0.0;
-            foreach (var item in items)
-            {
-                if (item == "") return "Error: empty barcode";
-                if (item == "99999") return "Error: barcode not found";
-                totalPrice += itemsPrices[item];
-            }
-
-            return $"${totalPrice.ToString("F" + 2)}";
-        }
-
         private static Dictionary<string, double> ItemsPrices()
         {
             var itemsPrices = new Dictionary<string, double>
@@ -70,6 +70,42 @@ namespace PointOfSaleKata
                 {"23456", 12.50}
             };
             return itemsPrices;
+        }
+
+        public string Scan(string barcode)
+        {
+            var itemsPrices = ItemsPrices();
+            var items = GetItems(barcode);
+            if (BarCodesAreNotValid(items, out var error)) return error;
+
+            var totalPrice = GetTotalPrice();
+            return ConvertPriceToPrint();
+
+            string ConvertPriceToPrint() => $"${totalPrice.ToString("F" + 2)}";
+            double GetTotalPrice() => items.Sum(i => itemsPrices[i]);
+        }
+
+        private static string[] GetItems(string barcode)
+        {
+            return barcode.Split(',');
+        }
+
+        private static bool BarCodesAreNotValid(string[] items, out string error)
+        {
+            if (items.Contains("99999"))
+            {
+                error = "Error: barcode not found";
+                return true;
+            }
+
+            if (items.Contains(string.Empty))
+            {
+                error = "Error: empty barcode";
+                return true;
+            }
+
+            error = "";
+            return false;
         }
     }
 }
